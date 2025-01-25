b0581cf7b5d811db58dabc81ae10724c8a2ef2e0
MinUk <qjvk2880@gmail.com>
2025-01-25 23:56:15 +0900

    변수 실습

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
-	}
-
+	const (
+		Apple  = iota // 0
+		Grape         // 1
+		Orange        // 2
+	)
 }
